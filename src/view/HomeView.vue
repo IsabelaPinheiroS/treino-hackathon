389b0ee0dc58673941c28e6edcd74b08f64f24ac
Isabela <bela_ps@hotmail.com>
2022-03-18 13:31:59 -0300

    Mudando texto

--- src/view/HomeView.vue
+++ src/view/HomeView.vue
@@ -3,14 +3,10 @@
     <h1 class="text-center mb-10 mt -10">Universidades</h1>
     <v-img class="img mb-10" src="../assets/university.jpg"> </v-img>
     <p class="text-center">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum nostrum et
-      perferendis laudantium laborum vel voluptate, ea in debitis voluptatum
-      pariatur ex rerum temporibus nihil officia, velit, aliquam cumque
-      placeat?<br /><br />
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae tempore
-      tempora quam. Suscipit ad inventore, cum temporibus repudiandae a
-      laboriosam, eaque dignissimos facere sit amet nam? Sequi veniam illo
-      corporis?
+     Olá! Seja bem vindo(a) a nossa aplicação! Aqui estão organizadas em 
+     uma tabela os nomes e os sites das melhores Reino Unido e seus respectivos 
+     sites na internet. Os nomes constam em ordem alfabética. Esperamos que 
+     assim fique fácil a sua visualização. =) <br>
     </p>
   </v-container>
 </template>
