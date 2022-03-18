72163dba59712b0a7429bd7ed2f3bcadf7812c5e
tanurematheus <tanure.matheus@gmail.com>
2022-03-18 10:53:25 -0300

    homeview

--- src/view/HomeView.vue
+++ src/view/HomeView.vue
@@ -1,5 +1,22 @@
 <template>
   <v-container>
-    <h1>teste</h1>
+    <h1 class="text-center mb-10 mt -10">Universidades</h1>
+    <v-img class="img mb-10" src="../assets/university.jpg"> </v-img>
+    <p class="text-center">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum nostrum et
+      perferendis laudantium laborum vel voluptate, ea in debitis voluptatum
+      pariatur ex rerum temporibus nihil officia, velit, aliquam cumque
+      placeat?<br /><br />
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae tempore
+      tempora quam. Suscipit ad inventore, cum temporibus repudiandae a
+      laboriosam, eaque dignissimos facere sit amet nam? Sequi veniam illo
+      corporis?
+    </p>
   </v-container>
 </template>
+
+<style scoped>
+.img {
+  border-radius: 10px;
+}
+</style>
