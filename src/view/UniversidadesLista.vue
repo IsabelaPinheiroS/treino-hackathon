f395cce77b290e7695a3083de615a79969aa0d6f
Bianca <bianca-franco1@hotmail.com>
2022-03-18 11:15:50 -0300

    Atualizações na view Lista

--- src/view/UniversidadesLista.vue
+++ src/view/UniversidadesLista.vue
@@ -1,7 +1,6 @@
 <template>
   <v-container>
     <h2 class="text-h5 text-center mb-3 mt-5">Lista de Universidades</h2>
-    <ClubesTabela :clubesOrdenados="clubesOrdenados"/>
   </v-container>
 </template>
 
@@ -24,14 +23,8 @@ export default {
       .then(json => {
         this.universidadeLista = json;
       })
-  },
-  computed: {
-    clubesOrdenados() {
-      const computado = this.clubesLista.slice(0).sort(
-        (a, b) =>  a.pontos > b.pontos ? -1 : 1
-      );
-      return computado;
-    }
   }
+
+
 }
 </script>
