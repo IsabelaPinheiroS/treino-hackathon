2a6f07091a27d92ae882d268be0132a43dab74e5
Isabela <bela_ps@hotmail.com>
2022-03-18 10:58:25 -0300

    Adicionando a Tool Bar

---
+++ src/components/shared/AppTopBar.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="app-top-bar">
+        <v-app-bar color="light-blue darken-3" dark>
+            <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Lista Universidades</v-toolbar-title>
+    </v-app-bar>
+
+    <v-navigation-drawer
+      v-model="drawer"
+      absolute
+      temporary
+    >
+      <v-list
+        nav
+        dense
+      >
+        <v-list-item-group
+          v-model="group"
+          active-class="light-blue lighten-3"
+        >
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-account</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Universidades</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+</template>
+
+
+<script>
+export default {
+    data: () => ({
+      drawer: false,
+      group: null,
+    }),
+    
+}
+</script>
+
+<style scoped>
+
+</style>
