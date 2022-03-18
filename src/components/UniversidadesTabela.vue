829eb145764260008ad33701afcc3c9cb19c3dfd
Felipe Cardoso Duarte <lipecd91@gmail.com>
2022-03-18 11:31:40 -0300

    alteração tabela

---
+++ src/components/UniversidadesTabela.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-simple-table>
+    <template v-slot:default>
+      <thead>
+        <tr>
+          <th class="text-left">
+            Universidade
+          </th>
+          <th class="text-left">
+            País
+          </th>
+          <th class="text-left">
+            Site
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="universidade in universidades"
+          :key="universidade.name"
+        >
+          <td>{{ universidade.name }}</td>
+          <td>{{ universidade.country }}</td>
+          <td>{{ universidade.web_pages }}</td>
+        </tr>
+      </tbody>
+    </template>
+  </v-simple-table>
+</template>
+
+<script>
+export default {
+    name: 'UniversidadesTabela',
+    props: {
+        universidades: {
+        type: Array,
+        required: true
+        }
+    }
+}
+</script>
+    
+<style>
+
+</style>
